7bf7bbad950a63a90836a41dabec28ca5aadec53
carmenales <carmen.aleslopez@gmail.com>
2026-06-16 13:10:32 +0200

    feat: dockerize preparadortai legacy app

--- apps/preparadortai/includes/config.php
+++ apps/preparadortai/includes/config.php
@@ -1,16 +1,17 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'preparadortai');
- 
+define('DB_SERVER', getenv('DB_SERVER') ?: 'localhost');
+define('DB_USERNAME', getenv('DB_USERNAME') ?: 'root');
+define('DB_PASSWORD', getenv('DB_PASSWORD') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'preparadortai');
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
-if($link === false){
+if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
 mysqli_set_charset($link, "utf8mb4");
 ?>
